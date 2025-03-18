ae784edb0cd5c45b7240c6d4c7df9f4721de6794
Serkan Bakırcı <75429747+Serkan-666@users.noreply.github.com>
2025-03-18 10:56:11 +0300

    # added function multiply2

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,6 @@ import Serkanls from './NativeSerkanls';
 export function multiply(a: number, b: number): number {
   return Serkanls.multiply(a, b);
 }
+export function multiply2(a: number, b: number): number {
+  return a + b;
+}
