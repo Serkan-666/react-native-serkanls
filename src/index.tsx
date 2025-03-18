a40e50a935ac4b17c94cba797a97a2810abf278f
Serkan Bakırcı <75429747+Serkan-666@users.noreply.github.com>
2025-03-18 15:47:38 +0300

    chore: index

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import Serkanls from './NativeSerkanls';
 export function multiply(a: number, b: number): number {
   return Serkanls.multiply(a, b);
 }
-export function multiply2(a: number, b: number): number {
-  return a + b;
-}
+export const nfc_start = async (nfcManager: any, NfcTech: any) => {
+  try {
+    await nfcManager.start();
+    await nfcManager.requestTechnology(NfcTech.IsoDep, {
+      alertMessage: 'Lütfen kimlik kartınızı cihaza yaklaştırın',
+    });
+
+    return 'NFC Başlatıldı.';
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
